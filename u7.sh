d77af3a7afff505096b7f18b273628487ff06f94
unknown <abhisheksrivastava9719@gmail.com>
2022-06-03 06:52:26 +0530

    UC8

--- u7.sh
+++ u7.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-arr=("${compute[$first]}" "${compute[$second]}" "${compute[$third]}" "${compute[$fourth]}" )
+arr=(${compute[$first]} ${compute[$second]} ${compute[$third]} ${compute[$fourth]} )
 #example set arr=("one" "two" "three")
 echo ${arr[@]}
